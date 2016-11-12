e0ff833e381d75c1cbbde07e6a9432cda6cf0b05
Miguel Barrenechea <miguel.barrenechea@vizzuality.com>
2016-11-12 04:45:49 +0100

    Header: improvements

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -7,33 +7,23 @@ class Header extends React.Component {
     super(props);
 
     this.state = {
-      toggleTools: false,
-      toggleSearch: false,
-      toggleMenu: false
+      active: ''
     };
 
     // Set bindings
-    this.onClickBtnTools = this.onClickBtnTools.bind(this);
-    this.onClickBtnSearch = this.onClickBtnSearch.bind(this);
-    this.onClickBtnMenu = this.onClickBtnMenu.bind(this);
+    this.onClickBtnAction = this.onClickBtnAction.bind(this);
   }
 
   /**
    * UI EVENTS
-   * - onClickBtnTools
-   * - onClickBtnSearch
-   * - onClickBtnMenu
+   * - onClickBtnAction
   */
-  onClickBtnTools() {
-    this.setState({ toggleTools: !this.state.toggleTools });
-  }
+  onClickBtnAction(e) {
+    const current = e.currentTarget.dataset.active;
 
-  onClickBtnSearch() {
-    this.setState({ toggleSearch: !this.state.toggleSearch });
-  }
-
-  onClickBtnMenu() {
-    this.setState({ toggleMenu: !this.state.toggleMenu });
+    this.setState({
+      active: (current !== this.state.active) ? current : ''
+    });
   }
 
   render() {
@@ -42,7 +32,7 @@ class Header extends React.Component {
         <nav>
           <ul className="list">
             <li>
-              <button className={`c-header-button ${this.state.toggleTools && '-active'}`} onClick={this.onClickBtnTools}>
+              <button data-active="tools" className={`c-header-button ${this.state.active === 'tools' && '-active'}`} onClick={this.onClickBtnAction}>
                 <svg className="c-icon"><use xlinkHref="#icon-grid" /></svg>
                 <span>Tools</span>
               </button>
@@ -55,12 +45,12 @@ class Header extends React.Component {
 
           <ul className="list">
             <li>
-              <button className={`c-header-button ${this.state.toggleSearch && '-active'}`} onClick={this.onClickBtnSearch}>
+              <button data-active="search" className={`c-header-button ${this.state.active === 'search' && '-active'}`} onClick={this.onClickBtnAction}>
                 <svg className="c-icon"><use xlinkHref="#icon-search" /></svg>
               </button>
             </li>
             <li>
-              <button className={`c-header-button ${this.state.toggleMenu && '-active'}`} onClick={this.onClickBtnMenu}>
+              <button data-active="menu" className={`c-header-button ${this.state.active === 'menu' && '-active'}`} onClick={this.onClickBtnAction}>
                 <svg className="c-icon"><use xlinkHref="#icon-menu" /></svg>
               </button>
             </li>
