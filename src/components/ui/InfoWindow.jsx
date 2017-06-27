e7e53450134b2c8f1ea738a84fdbd6c88966e2fa
Andrés González Muñoz <andres.gnlez@gmail.com>
2017-06-27 16:01:07 +0200

    Adds units to infowindow layer

--- src/components/ui/InfoWindow.jsx
+++ src/components/ui/InfoWindow.jsx
@@ -2,15 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default function InfoWindow(props) {
-  const { country } = props;
+  const { country, unit } = props;
 
   return (
     <div className="c-infowindow">
       <h3>{country}</h3>
+      {unit && <h3>({unit})</h3>}
     </div>
   );
 }
 
 InfoWindow.propTypes = {
-  country: PropTypes.string
+  country: PropTypes.string,
+  unit: PropTypes.string
 };
