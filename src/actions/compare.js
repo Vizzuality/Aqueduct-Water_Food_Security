5b93ce5b528d616182ac84ae30d598631a40a706
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2019-06-27 11:18:46 +0200

    removes old code

--- src/actions/compare.js
+++ src/actions/compare.js
@@ -1,16 +1,10 @@
 import {
-   COMPARE_SET_COUNTRY,
-   COMPARE_SET_FILTER,
-   COMPARE_EMPTY_COUNTRIES
-}
-from 'constants/compare';
+  COMPARE_SET_COUNTRY,
+  COMPARE_EMPTY_COUNTRIES
+} from 'constants/compare';
 
 export function setCompareCountry(country) {
   return dispatch => dispatch({ type: COMPARE_SET_COUNTRY, payload: country });
-}
-
-export function setCompareFilters(filter) {
-  return dispatch => dispatch({ type: COMPARE_SET_FILTER, payload: filter });
 }
 
 export function emptyCompareCountries() {
