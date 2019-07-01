5485bc66b240b99748852db9457dde7de86ba463
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2019-07-01 14:06:40 +0200

    Fix routes

--- config/environments/production.js
+++ config/environments/production.js
@@ -3,6 +3,7 @@ const express = require('express');
 
 const indexPath = path.join(process.cwd(), 'dist/index.html');
 
+
 module.exports = (app) => {
   app.use(express.static(path.join(process.cwd(), 'dist')));
   app.get('*', (req, res) => {
