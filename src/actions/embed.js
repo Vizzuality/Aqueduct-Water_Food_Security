6f02b5ca0e639d7e357828bbd4476cbf1b9f1a53
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2019-07-01 17:17:37 +0200

    Fixes embed

--- src/actions/embed.js
+++ src/actions/embed.js
@@ -1,8 +1,7 @@
-import { Deserializer } from 'jsonapi-serializer';
+import axios from 'axios';
+import WRISerializer from 'wri-json-api-serializer';
 import { store } from 'main';
 import { SET_EMBED, GET_WIDGET_SUCCESS, GET_WIDGET_ERROR } from 'constants/embed';
-
-const deserializer = new Deserializer({ keyForAttribute: 'camelCase' });
 
 export function setEmbed(embed) {
   return {
@@ -14,17 +13,12 @@ export function setEmbed(embed) {
 export function getWidget() {
   return (dispatch) => {
     const state = store.getState();
-    fetch(`${config.API_URL}/dataset/${state.embed.id}?app=aqueduct&includes=widget`)
-      .then(res => res.json())
-      .then((data) => {
-        deserializer.deserialize(data, (err, widgetData) => {
-          if (err) throw new Error('Error deserializing json api');
-          if (!widgetData.widget.length) return;
-
-          dispatch({
-            type: GET_WIDGET_SUCCESS,
-            payload: widgetData.widget[0].attributes
-          });
+    axios.get(`${config.API_URL}/dataset/${state.embed.id}?app=aqueduct&includes=widget`)
+      .then(({ data }) => {
+        const dataSerialized = WRISerializer(data);
+        dispatch({
+          type: GET_WIDGET_SUCCESS,
+          payload: dataSerialized.widget.length ? dataSerialized.widget[0] : null
         });
       })
       .catch((err) => {
--- src/components/pages/EmbedPage.jsx
+++ src/components/pages/EmbedPage.jsx
@@ -10,6 +10,8 @@ export default class EmbedPage extends React.Component {
   }
 
   render() {
+    console.log(this.props);
+
     return (
       <div className="l-embed l-fullheight">
         {this.props.error}
