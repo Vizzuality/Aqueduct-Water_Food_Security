4be3339f60946bc88ae2337697d4512a06e004af
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2019-07-09 13:10:27 +0200

    Adds another vocabulary

--- src/selectors/widgets_active.js
+++ src/selectors/widgets_active.js
@@ -18,7 +18,7 @@ const getActiveWidgets = (_datasets, _filters, _compare) => {
         ...(dataset.metadata && dataset.metadata.length) && { metadata: dataset.metadata[0] }
       };
       // NOTE: legacy vocabulary stores former used tags
-      const vocabulary = dataset.vocabulary.find(v => v.name === 'legacy');
+      const vocabulary = dataset.vocabulary.find(v => v.name === 'legacy' || v.name === 'attributes');
       const datasetTags = vocabulary ? vocabulary.tags : [];
       const cropFilter = datasetTags.includes(cropScope);
       const geographyFilter = datasetTags.includes(geographyScope);
