9a4a145a8816884db682c89f9e5288d804912543
Andrés González <andres.gonzalez@vizzuality.com>
2019-08-01 16:29:57 +0200

    Updates app header

--- src/routes.jsx
+++ src/routes.jsx
@@ -17,9 +17,9 @@ import { onEnterMapPage, onEnterComparePage, onEnterEmbedPage, onEnterReportPage
 const Routes = ({ history }) => (
   <Router history={history}>
     <Route path="/" component={App}>
-      <IndexRoute components={{ header: () => <Header title="Food" />, main: MapPage }} onEnter={onEnterMapPage} />
+      <IndexRoute components={{ header: () => <Header title="Food" currentApp="water-and-food-security-analyzer" />, main: MapPage }} onEnter={onEnterMapPage} />
       <Route path="compare">
-        <IndexRoute components={{ header: () => <Header title="Food" />, main: ComparePage }} onEnter={onEnterComparePage} />
+        <IndexRoute components={{ header: () => <Header title="Food" currentApp="water-and-food-security-analyzer" />, main: ComparePage }} onEnter={onEnterComparePage} />
       </Route>
       <Route path="embed">
         <IndexRoute components={{ main: EmbedPage }} onEnter={onEnterEmbedPage} />
