374b87d4d1185711d3e3338196fc52601cb8a0d8
David Inga <david.inga@vizzuality.com>
2019-10-17 14:40:37 +0200

    fixed url for report modal

--- src/components/modal/MapPDFModal.jsx
+++ src/components/modal/MapPDFModal.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 
-export default class MapPDFModal extends React.Component {
-  render() {
-    return (
-      <iframe
-        className="c-iframe -responsive"
-        width={800}
-        height={800}
-        src={`/#/report${window.location.search}`}
-      />
-    );
-  }
-}
+const MapPDFModal = () => {
+  const { location: { origin, pathname, hash } } = window;
+  const { length } = hash;
+  const iframeURL = `${origin}${pathname}#/report${hash.slice(2, length)}`;
+
+  return (
+    <iframe
+      title="Report modal"
+      className="c-iframe -responsive"
+      width={800}
+      height={800}
+      src={iframeURL}
+    />
+  );
+};
 
 MapPDFModal.propTypes = {
 };
+
+export default MapPDFModal;
