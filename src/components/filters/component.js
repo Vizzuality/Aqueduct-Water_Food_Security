0b2c19d42cd8aa88b70896cc96ef64dd62db7cac
Jonathan Goley <jgoley@gmail.com>
2021-06-14 14:05:35 -0400

    [#178307580] Re-enable Water Risk selection and remove "All Crops" from crop selector

--- src/components/filters/component.js
+++ src/components/filters/component.js
@@ -316,7 +316,7 @@ class Filters extends PureComponent {
 
                 <div className="small-12 medium-4 columns">
                   {/* Water */}
-                  <div className="c-filters-item disabled" title="This filter is temporarily unavailable.">
+                  <div className="c-filters-item">
                     <div className="filter-item-header">
                       <span className="title">Water Risk</span>
                       <button
@@ -332,10 +332,13 @@ class Filters extends PureComponent {
                     </div>
 
                     <CustomSelect
-                      options={[{category: "water", label: "Disabled", name: "Disabled", value: "disabled" }]}
-                      className='-disabled'
-                      value={'disabled'}
-                      onValueChange={(selected) => { }}
+                      options={waterOptions}
+                      value={filters.indicator}
+                      onValueChange={(selected) => {
+                        this.handleWaterRiskIndicator(selected);
+                        setLayerParametrization({ opacity: 1 });
+                        if (selected.value) logEvent('[AQ-Food] Map', 'select water risk indicator', selected.label);
+                      }}
                     />
                   </div>
                 </div>
