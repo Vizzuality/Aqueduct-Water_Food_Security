fc6d621ed11d47a1c055f6489bdabe95cfd5af44
Zach Parks <zparks88@gmail.com>
2021-11-18 10:27:53 -0500

    Remove unused file

--- src/utils/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export const ANALYSIS_API = axios.create({
-  baseURL: 'http://localhost:5100/api/v1/aqueduct/analysis/food-supply-chain',
-  headers: { 'Content-Type': 'application/json' }
-});
-
-export default { ANALYSIS_API };
